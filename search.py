3e2efd4a7660ef67fc4e428bd0012957c9bc2756
Maxim Shelopugin <maxim.shelopugin@gmail.com>
2019-04-15 23:02:04 -0400

    added search

--- search.py
+++ search.py
@@ -4,9 +4,12 @@ import numpy as np
 def priority(U, v, q):
 	return 1 / np.linalg.norm(v - np.dot(U,q))
 
+def distance(x, q):
+	return np.linalg.norm(x-q)
+
 def guided_search(root, q, t, vectors):
 	#C_q = []
-	#B = binary_search_tree
+	B = [] # needs to be bst
 	A = None
 	ai = None
 		
@@ -34,14 +37,24 @@ def guided_search(root, q, t, vectors):
 # Calculate priority value, set count ++
 		count = count + 1
 		priority_value = priority(root.direction, root.median, q)
-		print(A)
-		#		Sort the rows of A in increasing order according to distance from q_not and let a have sorted indecies
-		#		Insert {ai(a(1))...ai(a(c''))} into B with count
-		#		struct = (count, current_node)
-		#		P.insert(struct, priority_value)
-		#	Leaf_q = indecies of S in leaf_node
-		#	C_q = C_q union leaf_q 
-		#	t = t - 1
-		#	current_node = (P.extract_max).current_node
-		#	P.extract_max.count delete from B
-		#return C_q union all candidates from B
+
+# Sort the rows of A in increasing order according to distance from q_not and let a have sorted indecies
+		distnaces = []
+		for i in A:
+			distnaces.append(distance(i, q_not))
+		a = [i[0] for i in sorted(enumerate(distnaces), key=lambda x:x[1])]
+
+# Set the tree with counts and indecies of points
+		for index in a:
+			B.append((ai[index],count))
+
+# Create a structire with count and put it into queue with priority
+		structure = (count,current_node)
+		P.put(priority_value, structure)
+
+	#	Leaf_q = indecies of S in leaf_node
+	#	C_q = C_q union leaf_q 
+	#	t = t - 1
+	#	current_node = (P.extract_max).current_node
+	#	P.extract_max.count delete from B
+	#return C_q union all candidates from B
